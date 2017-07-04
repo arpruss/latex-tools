5fc81c20a96f62b02a2aaf984eddcb8873604ece
arpruss <arpruss@gmail.com>
2017-07-04 10:39:40 -0500

    foo

--- begin-end.py
+++ begin-end.py
@@ -1,5 +1,9 @@
 import sys
 
+#
+# TODO: handle backslash escapes
+#
+
 def err(count, msg, context):
     sys.stdout.write("%d: %s\n" % (count,msg))
     sys.stdout.write("context: %s\n" % context)
@@ -11,36 +15,54 @@ def index(s, sub):
     except:
         return -1
 
+def beginEndMatcher(line):
+    while len(line):
+        begin = index(line,"\\begin{")
+        end = index(line,"\\end{")
+        if begin>=0 and (end<0 or end>begin):
+            line = line[begin+7:]
+            brace = index(line,"}")
+            if brace<0:
+                err(count, "Missing closing brace", line)
+            beginEndStack.append( (count,line[:brace]) )
+            line = line[brace+1:]
+        elif end>=0 and (begin<0 or begin>end):
+            line = line[end+5:]
+            brace = index(line,"}")
+            if brace < 0:
+                err(count, "Missing closing brace", line)
+            env = line[:brace]
+            if len(beginEndStack) == 0:
+                err(count, "\end{%s} without opening" % env, line)
+            elif beginEndStack[-1][1] != env:
+                err(count, "\begin{%s} in line %d doesn't match \end{%s}" % (beginEndStack[-1][1], beginEndStack[-1][0], env), line)
+            beginEndStack.pop()
+            line = line[brace+1:]
+        else:
+            line = ""
+        
+def braceMatcher(line):
+    for i,c in enumerate(line):
+        if c == '{' and (i==0 or line[i-1] != '\\'):
+            braceStack.append(count)
+        elif c == '}' and (i==0 or line[i-1] != '\\'):
+            if len(braceStack) == 0:
+                err(count, "Closing brace without matching opening", line)
+            else:
+                braceStack.pop()
+        
 for file in sys.argv[1:]:
     print(file)
-    stack = []
-    count = 0
+    beginEndStack = []
+    braceStack = []
+    count = 1
     with open(file) as f:
         for line in f:
-            while len(line):
-                begin = index(line,"\\begin{")
-                end = index(line,"\\end{")
-                if begin>=0 and (end<0 or end>begin):
-                    line = line[begin+7:]
-                    brace = index(line,"}")
-                    if brace<0:
-                        err(count, "Missing closing brace", line)
-                    stack.append( (count,line[:brace]) )
-                    line = line[brace+1:]
-                elif end>=0 and (begin<0 or begin>end):
-                    line = line[end+5:]
-                    brace = index(line,"}")
-                    if brace < 0:
-                        err(count, "Missing closing brace", line)
-                    env = line[:brace]
-                    if len(stack) == 0:
-                        err(count, "\end{%s} without opening" % env, line)
-                    elif stack[-1][1] != env:
-                        err(count, "\begin{%s} in line %d doesn't match \end{%s}" % (stack[-1][1], stack[-1][0], env), line)
-                    stack.pop()
-                    line = line[brace+1:]
-                else:
-                    line = ""
+            beginEndMatcher(line)
+            braceMatcher(line)
             count += 1
-        if len(stack) > 0:
-            err(count, "Stack still has %d elements; last one is \begin{%s} started in line %d" % (len(stack), stack[-1][1], stack[-1][0]), "EOF")
+        if len(beginEndStack) > 0:
+            err(count, "begin-end stack still has %d elements; last one is \begin{%s} started in line %d" % (len(beginEndStack), beginEndStack[-1][1], beginEndStack[-1][0]), "EOF")
+        if len(braceStack) > 0:
+            err(count, "brace stack still has %d elements; last one was started on line %d" % (len(braceStack), braceStack[-1]), "EOF")
+        
